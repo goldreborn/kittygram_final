96df4176428c2c2e639193b99d1ad873775ac25f
goldreborn <yakov.kuznetsov98@yandex.ru>
2024-04-23 13:26:51 +0300

    fix

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -8,7 +8,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('SECRET_KEY', get_random_secret_key())
 
-DEBUG = os.getenv('DEBUG') == any([x for x in ('true', 'True', True)])
+DEBUG = True
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', default='127.0.0.1, localhost').split(',')
 
