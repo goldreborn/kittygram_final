709310bb9519c42d16db81085d188a8485a6369f
goldreborn <yakov.kuznetsov98@yandex.ru>
2024-04-23 17:14:16 +0300

    fix

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -62,7 +62,7 @@ DATABASES = {
         'NAME': 'postgres',
         'USER': 'postgres',
         'PASSWORD': 'postgres',
-        'HOST': '127.0.0.1',
+        'HOST': 'kittygram-db-1',
         'PORT': 5432,
     }
 }
