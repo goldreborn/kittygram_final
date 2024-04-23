308f4ebdbf628e01172ac900b4dd827d4dd4478e
goldreborn <yakov.kuznetsov98@yandex.ru>
2024-04-23 18:59:38 +0300

    fix

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -60,8 +60,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'postgres',
-        'USER': 'adm',
-        'PASSWORD': 'adm',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
         'HOST': '127.0.0.1',
         'PORT': 5432,
     }
